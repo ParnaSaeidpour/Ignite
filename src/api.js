ff823a507bfa08725b0b714fa7975b7c70b3f656
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-12-27 09:49:51 +0100

    Check error

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 
 
-// API Key
 
-const Token="1ffa8ea614d649bc942025bd5ac29a24"
 
 
 // Base URL
@@ -13,22 +11,20 @@ const base_url= "https://api.rawg.io/api/games?key=1ffa8ea614d649bc942025bd5ac29
 // Getting the date
 
 const getcurMonth=()=>{
-    const today= new Date()
-    curMonth=today.getMonth() +1
-    if(curMonth<10){
-        return `0${curMonth}`;
+    const month= new Date().getMonth() +1
+    if(month<10){
+        return `0${month}`;
     }else{
-        return curMonth
+        return month
     }
 };
 
 const getcurDay=()=>{
-    const today= new Date()
-    curDay=today.getDate()
-    if(curDay<10){
-        return `0${curDay}`;
+    const day= new Date().getDate()
+    if(day<10){
+        return `0${day}`;
     }else{
-        return curDay
+        return day
     }
 };
 
@@ -39,7 +35,7 @@ const currentMonth = getcurMonth()
 const currentDay = getcurDay()
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
 const lastYear =`${currentYear-1}-${currentMonth}-${currentDay}`
-const nextYear =`${currentYear+1}-${currentMonth}-${currentDay}`
+//const nextYear =`${currentYear+1}-${currentMonth}-${currentDay}`
 
 // Popular Games
 const popular_games =`&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
