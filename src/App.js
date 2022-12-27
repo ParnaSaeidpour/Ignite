1ee9186ac60c8dd594490956942fc6bf953601b3
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-12-27 11:17:51 +0100

    Refactory to home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React,{useEffect} from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+import React from "react";
+import Home from "./pages/Home"
+
 
 
 function App() {
-  const dispatch= useDispatch()
-  useEffect(()=>{
-    dispatch(loadGames())
-  })
+ 
   return (
     <div>
       <h1 className='text-5xl italic text-blue-500 font-bold underline text-center'>Hi</h1>
-
+      <Home />
     </div>
   );
 }
