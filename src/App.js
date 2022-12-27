f182eccaa5dd7af2679591283df538369fa032e6
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-12-27 09:51:49 +0100

    Featch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
+
 
 function App() {
+  const dispatch= useDispatch()
+  useEffect(()=>{
+    dispatch(loadGames())
+  })
   return (
-    <div className="App">
-      <h1 className='text-5xl italic text-blue-500 font-bold underline'>Hi</h1>
+    <div>
+      <h1 className='text-5xl italic text-blue-500 font-bold underline text-center'>Hi</h1>
 
     </div>
   );
