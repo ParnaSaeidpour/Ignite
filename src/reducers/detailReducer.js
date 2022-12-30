b68cc4845e5df514547045b5e7477b248ce71f91
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-12-30 15:28:14 +0100

    Add scrren in detailreducer

--- src/reducers/detailReducer.js
+++ src/reducers/detailReducer.js
@@ -1,14 +1,17 @@
-const initialState ={
-    game:{}
+const initState ={
+    game:{},
+    screen:{}
 }
 
 
 
-const detailReducer =(state=initialState, action)=>{
+const detailReducer =(state=initState, action)=>{
     switch(action.type){
         case "GET_DETAIL":
-            return{...state,
-                game:action.payload.game
+            return{
+                ...state,
+                game: action.payload.game,
+                screen: action.payload.screen
 
             }
 
