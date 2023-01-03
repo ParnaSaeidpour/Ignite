258efc47bca7ad7fcbd3514ad9d7ca77e9a76985
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-01-03 09:12:57 +0100

    Use link

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,6 +6,7 @@ import React from "react";
 
 import { useDispatch } from "react-redux";
 import {loadDetail} from "../actions/detailAction"
+import { Link } from "react-router-dom";
 
 const Games=({name,released,image,id})=>{
     //load Detail
@@ -18,10 +19,12 @@ const Games=({name,released,image,id})=>{
 
 
     return(
-        <div onClick={loadDetailHandler}  className="shadow-2xl shadow-gray-400 text-center rounded-md overflow-hidden">
+        <div onClick={loadDetailHandler}  className="cursor-pointer shadow-2xl shadow-gray-400 text-center rounded-md overflow-hidden">
+            <Link to={`/game/${id}`}>
             <h3 className="italic text-black pt-3 text-lg">{name}</h3>
             <p className="pt-3 text-sm">{released}</p>
             <img src={image} alt={name} className=" object-cover h-full"/>
+            </Link>
         </div>
     )
 }
