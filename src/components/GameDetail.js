839140bc44a6bdf737ff21711a8a298e9d11802d
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-01-03 14:17:59 +0100

    Add Image

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -7,6 +7,15 @@ import { useSelector } from "react-redux";
 import {useNavigate} from  'react-router-dom'
 import { smallImage } from "../util";
 
+//IMAGES
+
+import playstation from "../img/playstation.svg"
+import steam from "../img/steam.svg"
+import xbox from "../img/xbox.svg"
+import nintendo from "../img/nintendo.svg"
+import apple from '../img/apple.svg'
+import gamepad from "../img/gamepad.svg"
+
 const GameDetail=()=>{
     
     const navigate=useNavigate()
@@ -19,6 +28,23 @@ const GameDetail=()=>{
             navigate('/')
         }
     }
+
+    // Get Platform Images 
+    const getPlatform =(platform)=>{
+        switch(platform){
+            case "playstation 4":
+                return playstation;
+            case "Xbox One":
+                return xbox;
+            case "PC":
+                return steam
+            case "Nintrndo Switch":
+                return nintendo;
+            case "iOS":
+                return apple;
+            default:
+                return gamepad
+    }}
     //Data
 
     const {game, screen,isLoading} =useSelector(state => state.detail)
@@ -26,17 +52,26 @@ const GameDetail=()=>{
         <>
         {!isLoading &&(
         <div onClick={exitDetailHandler} className=" drop-shadow-md shadow-inner fixed overflow-y-scroll w-full min-h-full top-0 left-0 overflow-hidden ">
-            <div className="bg-white  max-w-7xl rounded-2xl left-5 absolute pl-20 pr-20 " >
+            <div className="bg-white  max-w-7xl rounded-2xl left-5 absolute pl-20 pr-20 cursor-pointer" >
                 <div className="flex items-center justify-between">
                     <div className="">
                         <h3 className="font-medium">{game.name}</h3>
-                        <p>Rating: {game.rating}</p>
+                        <p>
+                        <div className="flex items-center">
+                                <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                                <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                                <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                                <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                        </div>
+                            Rating: {game.rating}
+                        </p>
                     </div>
                     <div className="items-center">
-                        <h3 className="font-medium">Platforms</h3>
-                        <div className="flex justify-evenly ml-12">
+                        <h3 className="font-medium ml-20">Platforms</h3>
+                        <div className="flex justify-between space-x-5 ">
                             {game.platforms?.map(data=>(
-                                <h3 key={data.platform.id}>{data.platform.name}</h3>
+                                <img key={data.platform.id} src={getPlatform(data.platform.name)} alt={data.platform.name}></img>
                             ))}
                         </div>
                     </div>
