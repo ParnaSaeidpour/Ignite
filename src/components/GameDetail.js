d6f1e3c050b5efad774879f419aa16791022e6eb
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-01-03 09:13:17 +0100

    create Gamedetail

---
+++ src/components/GameDetail.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+
+//Redux
+
+import { useSelector } from "react-redux";
+
+const GameDetail=()=>{
+    //Data
+
+    const {game, screen} =useSelector(state => state.detail)
+    return(
+        <div className="drop-shadow-md shadow-inner fixed overflow-y-scroll w-full min-h-full top-0 left-0 overflow-auto ">
+            <div className="bg-white  max-w-7xl rounded-2xl left-5 absolute pl-20 pr-20 " >
+                <div className="flex items-center justify-between">
+                    <div className="">
+                        <h3 className="font-medium">{game.name}</h3>
+                        <p>Rating: {game.rating}</p>
+                    </div>
+                    <div className="items-center">
+                        <h3 className="font-medium">Platforms</h3>
+                        <div className="flex justify-evenly ml-12">
+                            {game.platforms?.map(data=>(
+                                <h3 key={data.platform.id}>{data.platform.name}</h3>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-20 object-cover">
+                    <img src={game.background_image} alt={game.background_image}/>
+                </div>
+                <div className="mt-20 mb-10">
+                    <p>{game.description_raw}</p>
+                </div>
+                <div className="gallery">
+                    {screen.results?.map(screen=>(
+                        <img src={screen.image} key={screen.id} alt={screen.image}/>
+                    ))}
+                  
+                </div>
+            </div>
+                
+        </div>
+    
+
+    )
+
+}
+
+export default GameDetail
