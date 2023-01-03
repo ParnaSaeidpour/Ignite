37f2ca0f21d761ef6040e203852df204b950e97e
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-01-03 09:12:14 +0100

    Add rouret

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Home from "./pages/Home"
+import {Route,Routes} from 'react-router-dom'
 
 
 
 function App() {
  
   return (
-    <div>
+    <div className="App">
       <h1 className='text-5xl italic text-blue-500 font-bold underline text-center'></h1>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/game/:id" element={<Home />}></Route>
+      </Routes>
+      
     </div>
   );
 }
